790ca9376c6fc65cc43dabd48a3c39fa42e4362a
senior0023 <jun.jie0305@protonmail.com>
2020-08-12 20:17:46 -0700

    install lodash module & add 'Hide Completed', incompleteTasks feature

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import Task from './Task';
+import React, { useState } from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
+import _ from 'lodash';
+import Task from './Task';
 import TasksCollection from '/imports/api/tasks';
 import { TaskForm } from './TaskForm';
 
@@ -15,12 +16,35 @@ const toggleChecked = ({ _id, isChecked }) => {
 const deleteTask = ({ _id }) => TasksCollection.remove(_id);
 
 export const App = () => {
+  const filter = {};
 
-  const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch());
+  const [hideCompleted, setHideCompleted] = useState(false);
+
+  if (hideCompleted) {
+    _.set(filter, 'isChecked', { $ne: true });
+  }
+
+  const { tasks, incompleteTasksCount } = useTracker(() => ({
+    tasks: TasksCollection.find(filter, { sort: { createdAt: -1 } }).fetch(),
+    incompleteTasksCount: TasksCollection.find({ isChecked: { $ne: true }}).count()
+  }));
 
   return (
     <div className="simple-todos-react">
-      <h1>Welcome to Meteor!</h1>
+      <h1>Todo List ({ incompleteTasksCount })</h1>
+
+      <div className="filters">
+        <label>
+          <input
+            type="checkbox"
+            readOnly
+            checked={ Boolean(hideCompleted) }
+            onClick={ () => setHideCompleted(!hideCompleted) }
+            />
+            Hide Completed
+        </label>
+      </div>
+
       <ul className="tasks">
         { 
           tasks.map(task => 
